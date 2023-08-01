d75553e0d3508d1c45e61ffc15356897b4139be7
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-08-01 18:02:58 +0100

    create internet gateway

--- main.tf
+++ main.tf
@@ -1,21 +1,31 @@
 # Create a VPC
 resource "aws_vpc" "vscode_example_vpc" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
+# create a subnet
 resource "aws_subnet" "vscode_example_public_subnet" {
-    vpc_id = aws_vpc.vscode_example_vpc.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.vscode_example_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
 
-    tags = {
-        Name = "dev_public"
-    }
+  tags = {
+    Name = "dev_public"
+  }
+}
+
+# create internet_gateway
+resource "aws_internet_gateway" "vscode_example_internet_gateway" {
+  vpc_id = aws_vpc.vscode_example_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
 }
--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "vscode"
 }
